90aad0953141fa79b1970765bfdb6f385387ff1d
Adson Souza <ty.msn@hotmail.com>
2023-08-06 12:08:56 -0300

    Implementação das rules + guard + tenant no dominio de transactions

--- back-end/src/transactions/transactions.service.ts
+++ back-end/src/transactions/transactions.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { Transaction } from './entities/transaction.entity';
 import { InjectModel } from '@nestjs/sequelize';
+import { TenantService } from 'src/tenant/tenant/tenant.service';
 
 @Injectable()
 export class TransactionsService {
 
   constructor(
     @InjectModel(Transaction) 
-    private model: typeof Transaction
+    private model: typeof Transaction,
+    private tenantService: TenantService
     ) {}
 
   create(createTransactionDto) {
-    return this.model.create(createTransactionDto);
+    return this.model.create({
+      ...createTransactionDto,
+      account_id: this.tenantService.tenant.id
+    });
   }
 
   findAll() {
-    return this.model.findAll();
+    return this.model.findAll({
+      where: {
+        account_id: this.tenantService.tenant.id
+      }
+    });
   }
 }
