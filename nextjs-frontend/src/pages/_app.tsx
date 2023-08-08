77075568553ea674802771336210202b272b8848
Adson Souza <ty.msn@hotmail.com>
2023-08-07 23:25:06 -0300

    Ajuste

--- nextjs-frontend/src/pages/_app.tsx
+++ nextjs-frontend/src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppContext, AppProps } from 'next/app'
 import theme from '../utils/theme'
 import { useEffect } from 'react'
+import {
+  SSRKeycloakProvider,
+  SSRCookies,
+  getKeycloakInstance,
+} from "@react-keycloak/ssr";
+import { KEYCLOAK_PUBLIC_CONFIG } from '../utils/auth'
+import { parseCookies } from '../utils/cookies'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, cookies }: AppProps & {cookies: any}) {
 
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
@@ -14,11 +21,20 @@ function MyApp({ Component, pageProps }: AppProps) {
   })
   
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SSRKeycloakProvider persistor={SSRCookies(cookies)} keycloakConfig={KEYCLOAK_PUBLIC_CONFIG as any}>
+       <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SSRKeycloakProvider>
+   
   )
+}
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  return {
+    cookie: parseCookies(appContext.ctx.req)
+  }
 }
 
 export default MyApp
--- nextjs-frontend/src/pages/transactions.tsx
+++ nextjs-frontend/src/pages/transactions.tsx
@@ -1,11 +1,15 @@
-import { Column } from '@devexpress/dx-react-grid';
+import { DataGrid, GridColDef, GridRowsProp, GridToolbar } from '@mui/x-data-grid';
+
 import { Container, Typography } from '@mui/material';
-import type { GetServerSideProps, NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next';
 
-
-const columns : Column[] = [
-    
-]
+const columns: GridColDef[] = [
+  { field: 'payment_date', headerName: 'Data Pagamento', type:'date', width: 150},
+  { field: 'name', headerName: 'Nome',width: 250},
+  { field: 'category', headerName: 'Categoria',width: 200 },
+  { field: 'type', headerName: 'Operação',width: 200 },
+  { field: 'created_at', headerName: 'Criado em', type:'date', width: 150,},
+];
 
 const TransactionsPage: NextPage = (props: any) => {
   return (
@@ -13,15 +17,26 @@ const TransactionsPage: NextPage = (props: any) => {
         <Typography component="h1" variant='h4'>
             MInhas TRansações
         </Typography>
-     
-
+        <DataGrid 
+        autoHeight rows={[]} 
+        columns={columns} 
+        disableColumnSelector
+        disableDensitySelector
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
+          },
+        }}
+        />
     </Container>
   )
 }
 
 export default TransactionsPage
 
-export const getServerSideProps: GetServerSideProps = async(ctx) => {
+export const getServerSideProps: GetServerSideProps = async() => {
   // posso chamar capi, database ... tudo de um lado do servidor
   return{
     props: {
