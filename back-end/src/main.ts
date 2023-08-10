0b298c69dc89a13d874fcca9535b98ff2922b1e6
Adson Souza <ty.msn@hotmail.com>
2023-08-09 22:45:50 -0300

    Implementação da listagem e criação das transactions

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     errorHttpStatusCode: 422
   }))
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3001',
+    credentials: true, // Permite o uso de credenciais (cookies, autenticação, etc.)
+  };
+
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
